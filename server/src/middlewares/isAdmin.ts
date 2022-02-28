c51d558b27519eb1f19741dec53f67b5c157e868
Valentin Guyon <v.guyon@fraudio.com>
2022-02-28 16:15:32 +0100

    feat(middlewares): added middleware for admins and mods

---
+++ server/src/middlewares/isAdmin.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from 'jsonwebtoken';
+import { User } from "../models/User";
+
+const isAdmin = async (req: Request, res: Response, next: NextFunction) => {
+
+    // Check if user has a token
+    const token = req.header('auth-token');
+
+    if (token) {
+
+        // Check if token is valid
+        try {
+
+            //TODO: Check _id in token payload
+            const { _id } = jwt.verify(token, "qhzbfkq9ze4g9q5s4g9qz4f9vqe98g4zq9e84gqr");
+
+            const user = await User.findOne({_id})
+
+            if(!user) return res.status(401).json({message: 'You are not authenticated'})
+
+            // Check if user is admin
+            if(user.isAdmin === false || user.isAdmin === undefined) {
+                return res.status(403).json({message: 'You are not admin'});
+            }
+
+        } catch (err) {
+            return res.status(400).json({err});
+        }
+
+    } else return res.status(401).json({message: 'You are not authenticated'});
+
+    return next();
+
+};
+
+export default isAdmin;
