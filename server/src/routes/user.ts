48cb4cbf35553b04b2f5f2873785c92e277afd01
Valentin Guyon <v.guyon@fraudio.com>
2022-03-01 23:37:14 +0100

    feat(api): new route for reporting

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,6 +1,5 @@
 import { Request, Response, Router} from 'express';
 import { User } from "../models/User";
-import bcrypt from "bcryptjs";
 
 const router = Router();
 
@@ -31,36 +30,41 @@ router.patch('/:id', async (req: Request, res: Response) => {
             return res.status(403);
         }
 
-        const validPassword = await bcrypt.compare(req.body.userPassword, user.userHashedPassword);
-        if (!validPassword) {
-            //hash the password
-            const salt = await bcrypt.genSalt(10);
-            const userHashedPassword = await bcrypt.hash(req.body.userPassword, salt);
-            await User.updateOne({_id: req.params.id}, {$set: {
-                    userMail: user.userMail,
-                    userHashedPassword: userHashedPassword,
-                    userName: req.body.userName,
-                    isAdmin: user.isAdmin,
-                    isModerator: user.isModerator,
-                    userDescription: req.body.userDescription,
-                    isReported: user.isReported,
-                    isBanned: user.isBanned,
-                    userLiked: req.body.userLiked,
-                }})
-        } else {
-            await User.updateOne({_id: req.params.id}, {$set: {
-                    userMail: user.userMail,
-                    userHashedPassword: req.body.userPassword,
-                    userName: req.body.userName,
-                    isAdmin: user.isAdmin,
-                    isModerator: user.isModerator,
-                    userDescription: req.body.userDescription,
-                    isReported: user.isReported,
-                    isBanned: user.isBanned,
-                    userLiked: req.body.userLiked,
-                }})
+
+        await User.updateOne({_id: req.params.id}, {$set: {
+                userMail: user.userMail,
+                userHashedPassword: req.body.userPassword,
+                userName: req.body.userName,
+                isAdmin: user.isAdmin,
+                isModerator: user.isModerator,
+                userDescription: req.body.userDescription,
+                isReported: user.isReported,
+                isBanned: user.isBanned,
+                userLiked: req.body.userLiked,
+            }});
+
+        const updatedUser = await User.findById(req.params.id);
+        return res.status(200).json(updatedUser);
+    } catch (err) {
+        return res.status(500).json(err);
+    }
+
+});
+
+/* REPORT user by id */
+router.patch('/report/:id', async (req: Request, res: Response) => {
+
+    try{
+
+        const user = await User.findById(req.params.id);
+
+        if(!user) {
+            return res.status(404)
         }
 
+        await User.updateOne({_id: req.params.id}, {
+                isReported: true
+            });
 
         const updatedUser = await User.findById(req.params.id);
         return res.status(200).json(updatedUser);
